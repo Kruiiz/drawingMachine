2b46d1e50f5f32ce5460f29da3e914f5d412b016
Kruiiz <42588014+Kruiiz@users.noreply.github.com>
2020-03-25 12:37:10 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  background(245,106,180);
+
+  strokeWeight(5);
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed){
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+      line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+
+  function keyTyped(){
+
+    if (key === 's'){
+      console.log('key ${key} is being typed')
+
+      if (key === 's'){
+
+      }
+    }
+
+    return false;
+  }
+
 }
